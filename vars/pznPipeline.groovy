82cfb17c02467e7187a71c1892a183335e7be24f
vinijf01 <vinijumatul@gmail.com>
2024-10-15 17:15:42 +0700

    add pzn pipeline

---
+++ vars/pznPipeline.groovy
@@ -0,0 +1,18 @@
+def call(Map config){
+	if(config.type == "maven"){
+		mavenPipeline()
+	}else {
+			pipeline {
+			agent any
+			stages {
+				stage("Unsupported Pipeline") {
+					steps {
+						script {
+							echo("Unsupported Pipeline")
+						}
+					}
+				}
+			}
+		}
+	}
+}
